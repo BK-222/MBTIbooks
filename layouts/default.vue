a6420c7e5255ef6d7de85d79cb1b1994932bd9ec
BK-222 <letsgetjscoding@gmail.com>
2024-09-19 07:03:44 +0100

    INFP images.

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
       <slot></slot>
     </main>
     <footer>
+      <p>*currently works only with the following types: INFP, ENFP, INTP, ENTP.</p>
       <p>&copy; 2024 Personality App</p>
     </footer>
   </div>
